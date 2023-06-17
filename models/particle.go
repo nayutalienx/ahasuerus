0709b0c34fd55374cf84e98e34324c5da95d7d8b
nayutalienx <nayutalienx@gmail.com>
2023-06-18 05:53:25 +0600

    Переделал particle

--- models/particle.go
+++ models/particle.go
@@ -4,54 +4,66 @@ import (
 	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
-type ParticleType int
-
-const (
-	Circle ParticleType = iota
-)
-
 type Particle struct {
-	Pos    rl.Vector2
-	Color  rl.Color
-	Type   ParticleType
-	Radius float32
+	startPos rl.Vector2
+	pos      rl.Vector2
 
-	FadeSpeed float32
-	FallSpeed float32
+	texture    rl.Texture2D
+	scale      float32
+	rotation   float32
+	maxOpacity float32
+
+	fadeSpeed float32
+	moveSpeed float32
 
 	opacity          float32
 	opacityIncrement bool
-	yOffset          float32
+
+	moveDirection rl.Vector2
+}
+
+func NewParticle(
+	startPos rl.Vector2,
+	texture rl.Texture2D,
+	moveDirection rl.Vector2,
+	fadeSpeed, moveSpeed, scale, rotation, maxOpacity float32,
+) *Particle {
+	p := Particle{
+		startPos:      startPos,
+		pos:           startPos,
+		texture:       texture,
+		maxOpacity:    maxOpacity,
+		moveDirection: moveDirection,
+		fadeSpeed:     fadeSpeed,
+		moveSpeed:     moveSpeed,
+		scale:         scale,
+		rotation:      rotation,
+	}
+
+	return &p
 }
 
 func (p *Particle) Update(delta float32) {
 
 	if p.opacity <= 0 {
 		p.opacityIncrement = true
-		p.yOffset = 0
+		p.pos = p.startPos
 	}
 
-	if uint8(p.opacity) >= p.Color.A {
+	if uint8(p.opacity) >= uint8(p.maxOpacity) {
 		p.opacityIncrement = false
 	}
 
 	if p.opacityIncrement {
-		p.opacity += p.FadeSpeed * delta
+		p.opacity += p.fadeSpeed * delta
 	} else {
-		p.opacity -= p.FadeSpeed * delta
+		p.opacity -= p.fadeSpeed * delta
 	}
 
-	p.yOffset += p.FallSpeed * delta
+	p.pos = rl.Vector2Add(p.pos, rl.Vector2Scale(p.moveDirection, p.moveSpeed))
 
 }
 
 func (p *Particle) Draw() {
-	c := rl.NewColor(
-		p.Color.R,
-		p.Color.G,
-		p.Color.B,
-		uint8(p.opacity),
-	)
-
-	rl.DrawCircle(int32(p.Pos.X), int32(p.Pos.Y)+int32(p.yOffset), p.Radius, c)
+	rl.DrawTextureEx(p.texture, p.pos, p.rotation, p.scale, rl.White)
 }
